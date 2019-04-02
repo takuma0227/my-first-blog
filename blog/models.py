6b81564d48c9021221c6f08a123ae8bdcfde63cc
takuma0227 <takuma762724@gmail.com>
2019-04-03 03:01:06 +0900

    Modified templetes to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def pablish(self):
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
 
