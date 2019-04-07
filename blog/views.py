0751e5ad309ec4e200535499c9639e859a0a30cf
takuma0227 <takuma762724@gmail.com>
2019-04-08 01:17:47 +0900

    nemui

--- blog/views.py
+++ blog/views.py
@@ -15,10 +15,10 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-        post = form.save(commit=False)
-        post.author =  request.user
-        post.published_date = timezone.now()
-        post.save()
+            post = form.save(commit=False)
+            post.author =  request.user
+            post.published_date = timezone.now()
+            post.save()
         return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
